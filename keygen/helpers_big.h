2d114adf87215f51464077a4e444ab888f054672
tg90000 <czegelandras@gmail.com>
2022-09-29 13:43:31 +0200

    big keygen

--- keygen/helpers_big.h
+++ keygen/helpers_big.h
@@ -5,17 +5,6 @@
 #define ull unsigned long long 
 #define ll long long 
 
-// p % a
-void bigmod(mpz_t a_i, mpz_t p_i, mpz_t res){
-    mpz_t a, p;
-    mpz_init(a);
-    mpz_init(p);
-    mpz_set(a, a_i);
-    mpz_set(p, p_i);
-    while(mpz_cmp(a,p) < 0){mpz_sub(p, p, a);}
-    mpz_set(res, p);
-}
-
 // p-1 = 2^r * m, returns r
 ull find_m(mpz_t p_i, mpz_t m){
     mpz_t p;
@@ -30,7 +19,9 @@ ull find_m(mpz_t p_i, mpz_t m){
         mpz_tdiv_r_2exp(rem, p, r);
     }
     r--;
-    mpz_tdiv_q_2exp(m, p, r); // set m to p / 2^r
+    mpz_tdiv_q_2exp(m, p, r);                                   // set m to p / 2^r
+    mpz_clear(p);
+    mpz_clear(rem);
     return r;
 }
 
@@ -41,9 +32,7 @@ void fast_pow(mpz_t a_i, mpz_t m_i, mpz_t p, mpz_t res){
     mpz_init_set(m, m_i);
     mpz_init_set_d(res, 1);
     ull i = 0;
-    mpz_t n2exp;
-    mpz_init_set_ui(n2exp, 1);
-    while(mpz_cmp_ui(m, 0) > 0){ //(1 << i) <= m
+    while(mpz_cmp_ui(m, 0) > 0){ 
         if(mpz_fdiv_q_ui(m, m, 2) == 1){
             mpz_mul(res, res, a);
             mpz_mod(res, res, p);
@@ -51,6 +40,8 @@ void fast_pow(mpz_t a_i, mpz_t m_i, mpz_t p, mpz_t res){
         mpz_mul(a, a, a);
         mpz_mod(a, a, p);
     }
+    mpz_clear(a);
+    mpz_clear(m);
 }
 
 // Miller-Rabin until a < 2*logn
@@ -65,7 +56,6 @@ int primecheck(mpz_t p){
     mpz_t var;
     mpz_init_set(var, p);
     for(logp = 0; mpz_cmp_ui(var, 0) != 0; ++logp) mpz_fdiv_q_ui(var, var, 2);      // get num of binary digits
-    printf("logp: %llu\n============================\n", logp);
 
     mpz_t a_i, a_m, p_m1;
     mpz_init(p_m1);
@@ -76,42 +66,85 @@ int primecheck(mpz_t p){
         fast_pow(a_i, m, p, a_m); 
         if (mpz_cmp_ui(a_m, 1)==0)continue;
 
-        for (ull i = 0; i <= r; i++){               // (a^m)^(2^r)
-            if (i == r) return 0;                   // a^(2^r * m) != 1 mod p
-            if (mpz_cmp(a_m, p_m1)==0){                        // a^(2^r * m) == -1 mod p
+        for (ull i = 0; i <= r; i++){                           // (a^m)^(2^r)
+            if (i == r) {
+                mpz_clear(m);
+                mpz_clear(a_i);
+                mpz_clear(a_m);
+                mpz_clear(var);
+                mpz_clear(p_m1);
+                return 0;                                       // a^(2^r * m) != 1 mod p
+            }
+            if (mpz_cmp(a_m, p_m1)==0){                         // a^(2^r * m) == -1 mod p
                 mpz_mul(a_m, a_m, a_m);
                 mpz_mod(a_m, a_m, p);                           
-                if (mpz_cmp_ui(a_m, 1)!=0) return 0;             // a^(2^(r+1) * m) != 1 mod p
+                if (mpz_cmp_ui(a_m, 1)!=0) {
+                    mpz_clear(m);
+                    mpz_clear(a_i);
+                    mpz_clear(a_m);
+                    mpz_clear(var);
+                    mpz_clear(p_m1);
+                    return 0;                                   // a^(2^(r+1) * m) != 1 mod p
+                }
                 else break;
             }
             mpz_mul(a_m, a_m, a_m);
             mpz_mod(a_m, a_m, p);
         }
     }
-    return 1; // passes for each a
+    mpz_clear(m);
+    mpz_clear(a_i);
+    mpz_clear(a_m);
+    mpz_clear(var);
+    mpz_clear(p_m1);
+    return 1;                                                   // passes for each a
 }
 
-// mpz_t gcd(mpz_t a, mpz_t b){
-//     return (b==0) ? a : gcd(b, a % b);
-// }
+// checks only for relative primes
+int gcd(mpz_t a, mpz_t b){
+    if (mpz_cmp_ui(b, 0) == 0) return mpz_cmp_ui(a, 1)==0 ? 1 : 0;
+    mpz_t new_b;
+    mpz_init(new_b);
+    mpz_fdiv_r(new_b, a, b);                                    // new_b = a%b
+    return gcd(b, new_b);
+}
 
-// mpz_t gcd_ext(mpz_t a, mpz_t b, mpz_t *x, mpz_t *y){
-//     if (b == 0) {
-//         *x = 1;
-//         *y = 0;
-//         return a;
-//     }else {
-//         mpz_t g = gcd_ext(b, a % b, x, y);             // basic rec. gcd
-//         mpz_t x_prev = *x; 
-//         *x = *y;                                    // x = y
-//         *y = x_prev - (a / b) * (*y);               // y = gen
-//         return g;                                   // returns gcd, and preps x, y
-//     }
-// }
+// returns 1 if gcd is 1
+int gcd_ext(mpz_t a, mpz_t b, mpz_t x, mpz_t y){
+    if (mpz_cmp_ui(b, 0) == 0) {
+        mpz_set_ui(x, 1);
+        mpz_set_ui(y, 0);
+        return mpz_cmp_ui(a, 1)==0 ? 1 : 0;
+    }else {
+        mpz_t new_b;
+        mpz_init(new_b);
+        mpz_fdiv_r(new_b, a, b);                                // new_b = a%b
+        int g = gcd_ext(b, new_b, x, y);                        // basic rec. gcd
 
-// // returns x1 a solution, thats enough to generate d
-// mpz_t diophantine_keygen(mpz_t a, mpz_t b, mpz_t c){
-//     mpz_t x, y;
-//     mpz_t g = gcd_ext(a, b, &x, &y);                   // gonna be 1
-//     return ((x * c) + 2*b);                         // should be positive, adds as general solution
-// }
+        mpz_t x_prev;
+        mpz_init_set(x_prev, x); 
+        mpz_set(x, y);                                          // x = y
+
+        mpz_t a_b;
+        mpz_init(a_b);
+        mpz_fdiv_q(a_b, a, b);
+        mpz_mul(a_b, a_b, y);
+        mpz_sub(y, x_prev, a_b);
+        mpz_clear(a_b);
+        mpz_clear(x_prev);
+        return g;                                               // returns gcd, and preps x, y
+    }
+}
+
+// returns x1 a solution, thats enough to generate d
+int diophantine_keygen(mpz_t a, mpz_t b, mpz_t x1){ // c = 1
+    mpz_t x, y;
+    mpz_init(x);
+    mpz_init(y);
+    int g = gcd_ext(a, b, x, y);                                // gonna be 1   
+    mpz_mul_ui(b, b, 2);
+    mpz_add(x1, x, b);                                          // should be positive, adds as general solution
+    mpz_clear(x);
+    mpz_clear(y);
+    return g;
+}
--- keygen/keygen_big.c
+++ keygen/keygen_big.c
@@ -5,56 +5,92 @@
 #include "helpers_big.h"
 #define ull unsigned long long
 #define ll long long
-#define MINBITS 10
-#define MINKEYLEN 10
-#define MAXKEYLEN 20
+#define MINBITS 500
+#define MAXBITS 512
+#define MINKEYLEN 500
+#define MAXKEYLEN 512
+#define SEED 8192
 
 // returns n
-ull gen_keys(ull p, ull q, ull *e, ull *d){
-    ull n = p*q;
-    ull phi = (p-1) * (q-1);
+void gen_keys(mpz_t p, mpz_t q, mpz_t e, mpz_t d, mpz_t n){
+    mpz_mul(n, p, q);
+    mpz_t phi;
+    mpz_init(phi);
+    mpz_sub(phi, n, p);
+    mpz_sub(phi, phi, q);
+    mpz_sub_ui(phi, phi, 1);                            // (p-1)*(q-1) = n - p - q + 1
+
+    mpz_t e_gcd;
+    mpz_init_set_ui(e_gcd,1);
+    mpz_t phi_gcd;
+    mpz_init_set(phi_gcd, phi); 
+
+    gmp_randstate_t rstate;
+    gmp_randinit_mt(rstate);
+    ull seed = (1 << 24) * (rand() % SEED);
+    gmp_randseed_ui(rstate, seed);
+    mpz_t min;
+    mpz_init(min);
+    mpz_ui_pow_ui(min, 2, MINKEYLEN);
+    mpz_urandomb(e, rstate, MAXKEYLEN);
+    mpz_add(e, e, min);
+    if (mpz_even_p(e)) mpz_add_ui(e, e, 1);
+    int x = 0;
     do{
-        *e = rand() % (1 << MAXKEYLEN);
-        if ((*e)<MINKEYLEN || (*e)%2==0)continue; // e > (1 << 20)
-    }
-    while(gcd(*e, phi) != 1);
+        mpz_add_ui(e, e, 2);
+        mpz_set(e_gcd, e);
+        mpz_set(phi_gcd, phi);
+    }while(gcd(e_gcd, phi_gcd) != 1);
 
-    *d = diophantine_keygen(*e, phi, 1);
-
-    return p*q;
+    mpz_set(e_gcd, e);
+    mpz_set(phi_gcd, phi);
+    diophantine_keygen(e_gcd, phi_gcd, d);
+    gmp_randclear(rstate);
+    mpz_clears(phi, e_gcd, phi_gcd, min);
 }
 
-ull gen_prime(){
-    ull p = (1 << MINBITS) * (rand() % 128);
-    do{
-        p++;
-    }
-    while(!primecheck(p));
-
-    return p;
+// returns p
+void gen_prime(mpz_t p){
+    ull seed = (1 << 24) * (rand() % SEED);
+    gmp_randstate_t rstate;
+    gmp_randinit_mt(rstate);
+    gmp_randseed_ui(rstate, seed);
+    mpz_t min;
+    mpz_init(min);
+    mpz_ui_pow_ui(min, 2, MINBITS);
+    mpz_urandomb(p, rstate, MAXBITS);
+    mpz_add(p, p, min);
+    if (mpz_even_p(p)){mpz_add_ui(p, p, 1);}
+    do {
+        mpz_add_ui(p, p, 2);
+    } while (!primecheck(p)); 
+    gmp_randclear(rstate);
+    mpz_clear(min);
 }
 
 int main(){
-
-    // mpz_t p;
-    // mpz_init(p);
-    // mpz_set_str(p, "3130000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000001183811000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000313", 10);
-    // int pass = primecheck(p);
-    // printf("3130000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000001183811000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000313 pass: %d\n", pass);
-
     srand(time(NULL));
-    ull p = gen_prime();
-    ull q = gen_prime();
-    ull e, d, n;
-    n = gen_keys(p, q, &e, &d);
+    mpz_t p, q, e, d, n;
+    mpz_init(p);
+    mpz_init(q);
+    mpz_init(e);
+    mpz_init(d);
+    mpz_init(n);
+    gen_prime(p);
+    gen_prime(q);
+    gen_keys(p, q, e, d, n);
 
     FILE* f_encode;
     f_encode = fopen("../encoder/e_n.txt", "w");
-    fprintf(f_encode, "%llu\n%llu\n", e, n);
+    mpz_out_str(f_encode, 10, e);
+    fprintf(f_encode, "\n");
+    mpz_out_str(f_encode, 10, n);
     fclose(f_encode);
     FILE* f_decode;
     f_decode = fopen("../decoder/d_n.txt", "w");
-    fprintf(f_decode, "%llu\n%llu\n", d, n);
+    mpz_out_str(f_decode, 10, d);
+    fprintf(f_decode, "\n");
+    mpz_out_str(f_decode, 10, n);
     fclose(f_decode);
     
     return 0;
